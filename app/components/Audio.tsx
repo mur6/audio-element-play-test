8e901949113f739fc420bb466708a23ae66036aa
MURAKI Taichi <muraki.taichi@gmail.com>
2025-07-02 10:26:57 +0900

    AutoPlayAudio: fix

--- app/components/Audio.tsx
+++ app/components/Audio.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useImperativeHandle, useRef, useState } from "react";
 
-const beep = new Audio("/audio/silent.mp3");
-beep.loop = true;
+const SILENT_SOUND_PATH = "/audio/silent.mp3";
 
 type Playlist = string[];
 
@@ -16,14 +15,12 @@ interface AutoPlayAudioProps {
 export function AutoPlayAudio({ ref }: AutoPlayAudioProps) {
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  useImperativeHandle(ref, () => ({
-    play: (playlist) => {
-      if (audioRef.current) {
-        audioRef.current.src = playlist[0];
-        audioRef.current.play();
-      }
-    },
-  }));
+  // TODO:
+  // - このコンポーネントは、生成されたタイミングから、常にSILENT_SOUND_PATHのサウンドを再生し続ける。
+  // - ただし、`ref`を通じて、外部から `playlist` (音声ファイルのリスト)が与えられたら、その音声ファイルを、リスト順に再生する。
+  // - `ref`を通じて、`play`メソッドを公開する。
+  // - `play`メソッドは、引数として受け取った `playlist` の音声ファイルを順番に再生する。
+  // - `playlist`の再生が終了したら、再びSILENT_SOUND_PATHの音声を再生する。
 
   return (
       <audio ref={audioRef} autoPlay preload="auto" style={{ display: 'none' }} />
