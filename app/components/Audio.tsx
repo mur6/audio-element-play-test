0f5b7a11497feb6cbed846f373ee6101e6fcc99f
MURAKI Taichi <muraki.taichi@gmail.com>
2025-07-02 09:58:33 +0900

    fix

--- app/components/Audio.tsx
+++ app/components/Audio.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useImperativeHandle, useRef, useState } from "react";
 
 
 type Playlist = string[];
 
 interface SimpleAudioRef {
-  start: () => void;
   play: (playlist: Playlist) => void;
 }
 
@@ -13,43 +12,16 @@ interface PlaylistProps {
 }
 
 export function SimpleAudio({ ref }: PlaylistProps) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
-  useEffect(() => {
-    if (audioRef.current) {
-      const audio = audioRef.current;
-
-      const handleAudioEnded = () => {
-        if (currentIndex + 1 < playlist.length) {
-          setCurrentIndex(currentIndex + 1);
-        } else {
-          // Uncomment this block if you want to loop the playlist
-          // setCurrentIndex(0)
-          // Uncomment the next line if you want to pause at the end of the playlist
-          // audio.pause()
-          // audio.currentTime = 0
-        }
-      };
-
-      audio.addEventListener("ended", handleAudioEnded);
-
-      return () => {
-        audio.removeEventListener("ended", handleAudioEnded);
-        audio.pause();
-        audio.currentTime = 0;
-      };
-    }
-  }, [currentIndex, playlist]);
-
-  useEffect(() => {
-    if (audioRef.current) {
-      const audio = audioRef.current;
-      audio.src = playlist[currentIndex];
-      audio.load();
-      audio.play();
-    }
-  }, [currentIndex, playlist]);
+  useImperativeHandle(ref, () => ({
+    play: (playlist) => {
+      if (audioRef.current) {
+        audioRef.current.src = playlist[0];
+        audioRef.current.play();
+      }
+    },
+  }));
 
   return <audio autoPlay controls ref={audioRef} style={{ display: "none" }} />;
 }
